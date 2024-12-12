a9e1c2aa71ad9fe8536830707e5f679db14fea29
Samir <abakhri.sd@gmail.com>
2024-12-12 16:47:54 +0500

    rest: server and few routes

---
+++ controllers/feed.js
@@ -0,0 +1,16 @@
+exports.getPosts = function (req, res, next) {
+  res.status(200).json({
+    posts: [{ title: 'First post', content: 'This is the first post'}]
+  })
+}
+
+exports.createPosts = function (req, res, next) {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Create db post
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: {id: new Date().toISOString().replaceAll(':', '')}
+  })
+}
